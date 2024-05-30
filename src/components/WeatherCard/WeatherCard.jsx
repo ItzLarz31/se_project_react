d32cfbbe966b83d68d83788e67ff0bf504f3ee73
ItzLarz31 <larz9889@hotmail.com>
2024-05-30 02:46:49 -0400

    Added in dynamic weathercards that change based off the current weather and have default options if I dont have a img card to suite the weather

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,24 +1,28 @@
 import "../WeatherCard/WeatherCard.css";
-import { weatherOptions } from "../../utils/constants";
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
 import sunnyDay from "../../assets/images/day/sunny.svg";
 
 function WeatherCard({ weatherData }) {
-  // const filteredOption = weatherOptions.filter((option) => {
-  //   return (
-  //     option.day === weatherData.isDay &&
-  //     option.condition === weatherData.condition
-  //   );
-  // });
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
 
-  // const weatherOption = filteredOption[0];
-  // debugger;
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+  }
 
   return (
     <section className="weather-card">
       <img
         className="weather-card__image"
-        src={sunnyDay}
-        alt="sunnyDay image"
+        src={weatherOption?.url}
+        alt={weatherOption.condition}
       />
       <p className="weather-card__temp">{weatherData.temp.F}&deg;F</p>
     </section>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,76 +1,75 @@
 export const weatherOptions = [
   {
     day: true,
-    condition: "clear",
-    url: new URL("../assets/images/day", import.meta.url).href,
-  },
-  {
-    day: true,
     condition: "cloudy",
-    url: new URL("../assets/images/day", import.meta.url).href,
+    url: new URL("../assets/images/day/cloudy.svg", import.meta.url).href,
   },
   {
     day: true,
     condition: "foggy",
-    url: new URL("../assets/images/day", import.meta.url).href,
+    url: new URL("../assets/images/day/foggy.svg", import.meta.url).href,
   },
   {
     day: true,
     condition: "rainy",
-    url: new URL("../assets/images/day", import.meta.url).href,
+    url: new URL("../assets/images/day/rainy.svg", import.meta.url).href,
   },
   {
     day: true,
     condition: "snowy",
-    url: new URL("../assets/images/day", import.meta.url).href,
+    url: new URL("../assets/images/day/snowy.svg", import.meta.url).href,
   },
   {
     day: true,
     condition: "stormy",
-    url: new URL("../assets/images/day", import.meta.url).href,
+    url: new URL("../assets/images/day/stormy.svg", import.meta.url).href,
   },
   {
     day: true,
     condition: "sunny",
-    url: new URL("../assets/images/day", import.meta.url).href,
+    url: new URL("../assets/images/day/sunny.svg", import.meta.url).href,
   },
 
   {
     day: false,
     condition: "clear",
-    url: new URL("../assets/images/night", import.meta.url).href,
+    url: new URL("../assets/images/night/clear.svg", import.meta.url).href,
   },
   {
     day: false,
     condition: "cloudy",
-    url: new URL("../assets/images/night", import.meta.url).href,
+    url: new URL("../assets/images/night/cloudy.svg", import.meta.url).href,
   },
   {
     day: false,
     condition: "foggy",
-    url: new URL("../assets/images/night", import.meta.url).href,
+    url: new URL("../assets/images/night/foggy.svg", import.meta.url).href,
   },
   {
     day: false,
     condition: "rainy",
-    url: new URL("../assets/images/night", import.meta.url).href,
+    url: new URL("../assets/images/night/rainy.svg", import.meta.url).href,
   },
   {
     day: false,
     condition: "snowy",
-    url: new URL("../assets/images/night", import.meta.url).href,
+    url: new URL("../assets/images/night/snowy.svg", import.meta.url).href,
   },
   {
     day: false,
     condition: "stormy",
-    url: new URL("../assets/images/night", import.meta.url).href,
-  },
-  {
-    day: false,
-    condition: "sunny",
-    url: new URL("../assets/images/night", import.meta.url).href,
+    url: new URL("../assets/images/night/stormy.svg", import.meta.url).href,
   },
 ];
+
+export const defaultWeatherOptions = {
+  day: {
+    url: new URL("../assets/images/day/sunny.svg", import.meta.url).href,
+  },
+  night: {
+    url: new URL("../assets/images/night/clear.svg", import.meta.url).href,
+  },
+};
 
 export const defaultClothingItems = [
   {
