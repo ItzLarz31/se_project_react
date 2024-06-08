5091f986869923478687bb54846876edee925946
ItzLarz31 <larz9889@hotmail.com>
2024-06-08 17:13:00 -0400

    Refactored AddItemModal to use ModalWithForm

--- src/components/AddItemModal/AddItemModal.jsx
+++ src/components/AddItemModal/AddItemModal.jsx
@@ -1,114 +1,61 @@
 import React from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import "./AddItemModal.css";
 
 function AddItemModal({ isOpen, title, closeActiveModal, onSubmit }) {
   return (
-    <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
-      <div className="modal__container">
-        <button
-          onClick={closeActiveModal}
-          type="button"
-          className="modal__close"
-        ></button>
-        <h2 className="modal__heading">{title}</h2>
+    <ModalWithForm
+      isOpen={isOpen}
+      title={title}
+      closeActiveModal={closeActiveModal}
+      onSubmit={onSubmit}
+    >
+      <label className="modal__input-label" htmlFor="card-title-input">
+        Name
+      </label>
+      <input
+        type="text"
+        name="title"
+        id="card-title-input"
+        placeholder="Name"
+        className="modal__input"
+        minLength="1"
+        maxLength="30"
+        required
+      />
+      <span className="modal__error" id="card-title-input-error"></span>
+      <label className="modal__input-label" htmlFor="card-url-input">
+        Image
+      </label>
+      <input
+        type="url"
+        name="url"
+        id="card-url-input"
+        placeholder="Image URL"
+        className="modal__input"
+        required
+      />
+      <span className="modal__error" id="card-url-input-error"></span>
+      <p className="modal__radio-label">Select the weather type:</p>
+      <div className="modal__radio-group">
+        <label>
+          <input type="radio" name="weather" id="weather-hot" value="Hot" />
+          Hot
+        </label>
+        <label>
+          <input type="radio" name="weather" id="weather-warm" value="Warm" />
+          Warm
+        </label>
+        <label>
+          <input type="radio" name="weather" id="weather-cold" value="Cold" />
+          Cold
+        </label>
+        <button type="submit" className="modal__save modal__save_disabled">
+          Add garment
+        </button>
       </div>
-    </div>
+    </ModalWithForm>
   );
 }
 
 export default AddItemModal;
-
-/* ------------------------------------------------------------------------------------ */
-/*      I HAVE TO LEAVE THIS FILE IN HERE OR AUTOCHECKER BUG WILL NOT LET ME SUBMIT     */
-/* ------------------------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------------------------ */
-/*                     Which is why I even refactored to begin with                     */
-/* ------------------------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------------------------ */
-/*       IF YOU KNOW A BETTER WAY TO BYPASS PLEASE TELL ME WITHOUT AUTO FAILING ME      */
-/* ------------------------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------------------------ */
-/*      ALSO I BELIEVE I FIXED YOUR OTHER REVIEW ISSUES! THANKS FOR THE FEEDBACK!      */
-/* ------------------------------------------------------------------------------------ */
-
-/* ------------------------------------------------------------------------------------ */
-/*           BELOW IS THE ONLY OTHER OPTION I CAN THINK OF THAT YOU MIGHT MEAN          */
-/* ------------------------------------------------------------------------------------ */
-
-// import React from "react";
-// import ModalWithForm from "../ModalWithForm/ModalWithForm";
-// import "./AddItemModal.css";
-
-// function AddItemModal({ isOpen, title, closeActiveModal, onSubmit }) {
-//   return (
-//     <ModalWithForm
-//       isOpen={isOpen}
-//       title={title}
-//       closeActiveModal={closeActiveModal}
-//       onSubmit={onSubmit}
-//     >
-//       <label className="modal__input-label" htmlFor="card-title-input">
-//         Name
-//       </label>
-//       <input
-//         type="text"
-//         name="title"
-//         id="card-title-input"
-//         placeholder="Name"
-//         className="modal__input"
-//         minLength="1"
-//         maxLength="30"
-//         required
-//       />
-//       <span className="modal__error" id="card-title-input-error"></span>
-//       <label className="modal__input-label" htmlFor="card-url-input">
-//         Image
-//       </label>
-//       <input
-//         type="url"
-//         name="url"
-//         id="card-url-input"
-//         placeholder="Image URL"
-//         className="modal__input"
-//         required
-//       />
-//       <span className="modal__error" id="card-url-input-error"></span>
-//       <p className="modal__radio-label">Select the weather type:</p>
-//       <div className="modal__radio-group">
-//         <label>
-//           <input type="radio" name="weather" id="weather-hot" value="Hot" />
-//           Hot
-//         </label>
-//         <label>
-//           <input type="radio" name="weather" id="weather-warm" value="Warm" />
-//           Warm
-//         </label>
-//         <label>
-//           <input type="radio" name="weather" id="weather-cold" value="Cold" />
-//           Cold
-//         </label>
-//         <button type="submit" className="modal__save modal__save_disabled">
-//           Add garment
-//         </button>
-//       </div>
-//     </ModalWithForm>
-//   );
-// }
-
-// export default AddItemModal;
-
-/* ------------------------------------------------------------------------------------ */
-/*                                   App.jsx addition                                   */
-/* ------------------------------------------------------------------------------------ */
-
-{
-  /* <ItemModal
-closeActiveModal={closeActiveModal}
-activeModal={activeModal}
-card={selectedCard}
-onClick={handelDeleteClick}
-/> */
-}
